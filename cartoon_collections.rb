d42352b233103b3e410441b875941bba054d5117
Brendan O'Dea <brendan.ode4@gmail.com>
2018-01-30 21:21:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(array)
 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  array.select{|item| cheese_types.include(item)}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
